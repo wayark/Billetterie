942fe12cfe5f246ad6d824a52f0fc8cf98dc00e8
mathisT <mathis.toinon@etu.univ-lyon1.fr>
2022-11-21 21:14:24 +0100

    add account management page

---
+++ src/view/v_accountManagement.php
@@ -0,0 +1,22 @@
+<?php require_once(PATH_VIEWS . 'header.php'); //header of the page
+?>
+    <link href="<?= PATH_CSS ?>accountManagement.css" rel="stylesheet">
+    <main>
+        <section>
+            <h1>Mon Compte</h1>
+            <div>
+                <img src="./asset/image/IMG_1394.jpg">
+                <div>
+                    <div class="nomPrenom">
+                        <h2>Prénom : Mathis</h2>
+                        <h2>Nom : Toinon</h2>
+                    </div>
+                    <h2>Email : toinon.math@gmail.com</h2>
+                    <button>Changer mon mot de passe</button>
+                </div>
+            </div>
+
+        </section>
+    </main>
+<?php require_once(PATH_VIEWS . 'footer.php'); //header of the page
+?>
