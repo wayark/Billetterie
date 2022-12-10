74cbe2332fa393fa0d33d9be6111c1af7253d024
SUKIRTHAN LINGET p2103150 <sukirthan.linget@etu.univ-lyon1.fr>
2022-12-10 19:10:43 +0100

    feat: add skeleton DTO event
    update EventDTO

---
+++ model/database/dto/EventDTO.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once PATH_MODELS . 'Event.php';
+
+class EventDTO extends DTO
+{
+    public function addEvent(Event $event) {
+
+    }
+
+    public function removeEvent(Event $event) {
+
+    }
+
+    public function updateEvent(Event $event) {
+
+    }
+}
