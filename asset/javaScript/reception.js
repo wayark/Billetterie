9b5d7371655a1783220facd7b569bbbbf231bc0e
Rayane Merlin <92400806+rayanemrln@users.noreply.github.com>
2023-01-05 21:23:56 +0100

    Href update on carousel

--- asset/javaScript/reception.js
+++ asset/javaScript/reception.js
@@ -2,16 +2,27 @@ var canClick = true;
 const moveCarousel = (button) => {
     if(canClick){
         let tendancies = document.querySelectorAll('.tendancies div');
+        let tendanciesContainer = document.querySelectorAll('.tendancies');
         if (button.id.includes('Left')) {
             let tmp = tendancies[0].cloneNode(true);
+            let tmpContainer = tendanciesContainer[0].cloneNode(true);
+
             tendancies[0].style.backgroundImage = tendancies[1].style.backgroundImage;
+            tendanciesContainer[0].href = tendanciesContainer[1].href;
             tendancies[1].style.backgroundImage = tendancies[2].style.backgroundImage;
+            tendanciesContainer[1].href = tendanciesContainer[2].href;
             tendancies[2].style.backgroundImage = tmp.style.backgroundImage;
+            tendanciesContainer[2].href = tmpContainer.href;
         } else {
             let tmp = tendancies[2].cloneNode(true);
+            let tmpContainer = tendanciesContainer[2].cloneNode(true);
+
             tendancies[2].style.backgroundImage = tendancies[1].style.backgroundImage;
+            tendanciesContainer[2].href = tendanciesContainer[1].href;
             tendancies[1].style.backgroundImage = tendancies[0].style.backgroundImage;
+            tendanciesContainer[1].href = tendanciesContainer[0].href;
             tendancies[0].style.backgroundImage = tmp.style.backgroundImage;
+            tendanciesContainer[0].href = tmpContainer.href;
         }
         canClick = false;
         setTimeout(() => {
