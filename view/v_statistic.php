e3bb86826274574fdd1c1f307aa839c38f7756e0
mathis toinon <toinon.math@gmail.com>
2023-01-06 14:20:21 +0100

    plot the curve as a function of the variable

--- view/v_statistic.php
+++ view/v_statistic.php
@@ -58,15 +58,22 @@ require_once './application/display/errorDisplay.php';
     </section>
 </main>
 <script>
+    tableau = [10, 20, 34, 55];
+    max = tableau[tableau.length - 1];
+    hauteurParVente = 150 /max;
+    console.log("hauteur : " + hauteurParVente);
+    longeureJour = 300 / 3;
+    console.log("longeure : " + longeureJour);
+    avanceJ = 0
     var ctx = document.getElementById('myCanvas').getContext('2d');
     ctx.strokeStyle = '#ff0000';
     ctx.lineWidth = 5;
-
+    
     ctx.beginPath();
-
-    ctx.moveTo(0, 0);
-
-    ctx.lineTo(300, 150);
+    console.log("point départ : x :" + avanceJ + "    y : " + tableau[0]*hauteurParVente)
+    ctx.moveTo(avanceJ, tableau[0]*hauteurParVente);
+    avanceJ = avanceJ + longeureJour
+    ctx.lineTo(avanceJ, tableau[1]*hauteurParVente);
 
     ctx.stroke();
 </script>
