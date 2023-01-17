ab39d50d4523c07bd9127a50e642e2e86227f61b
Rayane Merlin <92400806+rayanemrln@users.noreply.github.com>
2023-01-17 06:40:41 +0100

    End Summary Page

--- view/v_summary.php
+++ view/v_summary.php
@@ -9,6 +9,7 @@
         <p> évènements) :</p>
     </div>
     <div class="event-container">
+        <div class="first-bar bar"></div>
         <div class="event-purchased flex-column">
             <div class="nb-tickets-purchased flex-row">
                 <h1>-</h1>
@@ -18,17 +19,89 @@
                 <h1 class="to-modify">Nom de l'évènement</h1>
                 <h1 class="quote">"</h1>
             </div>
-            <div class="right-part-event grid-row">  
+            <div class="right-part-event flex-row">  
                 <a href="./?page=event&id=" class="link-to-event">
                     <img src="<?= PATH_IMAGES ;?>/events/wayark.jpg" alt="event" class="event-img">
                 </a>
-                <div class="event-info">
-
+                <div class="event-info flex-column">
+                    <h1 class="type-event">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, aliquam! Pariatur, alias! Vel, dolor?</h1>
+                    <div class="date-event-container flex-row">
+                        <h1 class="date-event">Le</h1>
+                        <h1 class="date-event">12/13/2003</h1>
+                    </div>
+                    <h1 class="adress-event">Adresse Evenement</h1>
+                    <h1 class="city-event">Ville Evenement</h1>
                     <form action="./?page=summary" method="post" class="button-generate-pdf">
-                        <button type="submit" name="generate-pdf">Generer PDF</button>
+                        <button type="submit" name="generate-pdf" class="flex-row">
+                            <p>Télécharger mon billet</p>
+                            <img src="<?= PATH_IMAGES ;?>/logos/pdf-icon.png" alt="">
+                        </button>
                     </form>
                 </div>
             </div>
+            <div class="bar"></div>
+        </div>
+        <div class="event-purchased flex-column">
+            <div class="nb-tickets-purchased flex-row">
+                <h1>-</h1>
+                <h1 class="to-modify">3</h1>
+                <h1>tickets pour :</h1>
+                <h1 class="quote">"</h1>
+                <h1 class="to-modify">Nom de l'évènement</h1>
+                <h1 class="quote">"</h1>
+            </div>
+            <div class="right-part-event flex-row">  
+                <a href="./?page=event&id=" class="link-to-event">
+                    <img src="<?= PATH_IMAGES ;?>/events/wayark.jpg" alt="event" class="event-img">
+                </a>
+                <div class="event-info flex-column">
+                    <h1 class="type-event">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, aliquam! Pariatur, alias! Vel, dolor?</h1>
+                    <div class="date-event-container flex-row">
+                        <h1 class="date-event">Le</h1>
+                        <h1 class="date-event">12/13/2003</h1>
+                    </div>
+                    <h1 class="adress-event">Adresse Evenement</h1>
+                    <h1 class="city-event">Ville Evenement</h1>
+                    <form action="./?page=summary" method="post" class="button-generate-pdf">
+                        <button type="submit" name="generate-pdf" class="flex-row">
+                            <p>Télécharger mon billet</p>
+                            <img src="<?= PATH_IMAGES ;?>/logos/pdf-icon.png" alt="">
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div class="bar"></div>
+        </div>
+        <div class="event-purchased flex-column">
+            <div class="nb-tickets-purchased flex-row">
+                <h1>-</h1>
+                <h1 class="to-modify">3</h1>
+                <h1>tickets pour :</h1>
+                <h1 class="quote">"</h1>
+                <h1 class="to-modify">Nom de l'évènement</h1>
+                <h1 class="quote">"</h1>
+            </div>
+            <div class="right-part-event flex-row">  
+                <a href="./?page=event&id=" class="link-to-event">
+                    <img src="<?= PATH_IMAGES ;?>/events/wayark.jpg" alt="event" class="event-img">
+                </a>
+                <div class="event-info flex-column">
+                    <h1 class="type-event">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, aliquam! Pariatur, alias! Vel, dolor?</h1>
+                    <div class="date-event-container flex-row">
+                        <h1 class="date-event">Le</h1>
+                        <h1 class="date-event">12/13/2003</h1>
+                    </div>
+                    <h1 class="adress-event">Adresse Evenement</h1>
+                    <h1 class="city-event">Ville Evenement</h1>
+                    <form action="./?page=summary" method="post" class="button-generate-pdf">
+                        <button type="submit" name="generate-pdf" class="flex-row">
+                            <p>Télécharger mon billet</p>
+                            <img src="<?= PATH_IMAGES ;?>/logos/pdf-icon.png" alt="">
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div class="last-bar bar"></div>
         </div>
     </div>
     <h1 class="subtitle-page">Waticket et ses membres vous remercient de votre confiance.</h1>
