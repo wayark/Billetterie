40ff450a82718dd7339df902876d5b836f9d30dd
SUKIRTHAN LINGET p2103150 <sukirthan.linget@etu.univ-lyon1.fr>
2023-01-19 23:25:45 +0100

    feat: delete event

--- controller/c_cart.php
+++ controller/c_cart.php
@@ -15,6 +15,10 @@ if(isset($_POST["addone"]) && isset($_POST["id"])){
     $_SESSION["cart"]->set($_POST["id"], $newQuantity);
 } else if(isset($_POST["delete"]) && isset($_POST["id"])) {
     $cartDTO = new CartDTO();
+    $pricingDAO = new TicketPricingDAO();
+    $pricing = $pricingDAO->getById($_POST["id"]);
+    $cartDTO->delete($pricing);
+    $_SESSION["cart"]->remove($_POST["id"], $_SESSION["cart"]->getInCartPricing()[$_POST["id"]]);
 }
 
 $presenter = new CartPresenter($_GET, $_POST);
