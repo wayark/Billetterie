21886ef73dfa36484520256272acd4dcdd611259
Rayane Merlin <92400806+rayanemrln@users.noreply.github.com>
2023-01-19 17:27:14 +0100

    Fix add to cart page

--- view/v_event.php
+++ view/v_event.php
@@ -23,6 +23,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= PATH_CSS ?>reception.css">
     <link rel="stylesheet" href="<?= PATH_CSS ?>event.css">
     <link rel="stylesheet" href="<?= PATH_MEDIA ?>Event.css">
     <script src="<?php echo PATH_SCRIPTS . "justbought.js" ?>" defer></script>
