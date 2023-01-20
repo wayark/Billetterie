780a234f16b7400c89ab3d01a9da17bb43da1267
Rayane Merlin <92400806+rayanemrln@users.noreply.github.com>
2023-01-20 01:09:30 +0100

    accents

--- model/PDFGenerator.php
+++ model/PDFGenerator.php
@@ -222,6 +222,10 @@ class PDFGenerator extends FPDF {
     }
 
     private function removeAccents(string $str) {
-        return iconv('UTF-8', 'ASCII//TRANSLIT', $str);
+        $str = htmlentities($str, ENT_NOQUOTES, 'utf-8');
+        $str = preg_replace('#\&([A-za-z])(?:acute|cedil|circ|grave|orn|ring|slash|th|tilde|uml)\;#', '\1', $str);
+        $str = preg_replace('#\&([A-za-z]{2})(?:lig)\;#', '\1', $str);
+        $str = preg_replace('#\&[^;]+\;#', '', $str);
+        return $str;
     }
 }
